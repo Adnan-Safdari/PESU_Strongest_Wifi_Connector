2ff86d0cbf4846f147862c7ee10128106d186344
Adnan-Safdari <safdariadnan@gmail.com>
2024-08-07 12:44:39 +0530

    Lists only PESU's APs

--- main.py
+++ main.py
@@ -19,18 +19,26 @@ def identifying_strongest_router():
     strongest_wifi_details = [{"ssid": "Sample", "bssid":"SampleMac", "singal":"-777", "auth":"SampleAuth"}]  # This sample data will get replaced
 
     # Looping through all the networks to find the strongest network
+    available_pesu_netowrks=[]
     for network in networks:
+        if network['ssid'] in ListPesuSSID():
+            available_pesu_netowrks.append({"ssid": network['ssid'], "bssid":network['bssid'], "signal":network['signal'], "auth":network['auth']})
+    
+
+    for network in available_pesu_netowrks:
+        print(network)
         if network['signal'] > strongest_band: # Lowest signal number is the strongest signal
             strongest_band = network['signal'] 
             strongest_wifi_details[0] = {"ssid": network['ssid'], "bssid":network['bssid'], "signal":network['signal'], "auth":network['auth']}
-            
-            #TODO: Check if the wifi SSID is a PESU Access Point
-    
-    print(f"strongest_wifi_details: {strongest_wifi_details}")
+    print("=======================================================")
+    print(f"The Router with the fastest connection is: {strongest_wifi_details}")    
+    print("=======================================================")
+
+
 
     #TODO: Connect to the fastest wifi access point
 
-# identifying_strongest_router()
+identifying_strongest_router()
 
 
-browser_login(cfgs.captive_portal_address) # Calling the selenium function and giving it the local ip to login to the portal
+# browser_login(cfgs.captive_portal_address) # Calling the selenium function and giving it the local ip to login to the portal
--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("config/Credentials.json", "r") as file:
-    data = json.load(file)
-
-print(data['username'])
